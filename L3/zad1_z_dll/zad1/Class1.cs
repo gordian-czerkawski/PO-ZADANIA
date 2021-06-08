8eb81adc5af779decf0a8388e83b52d25cea3733
Gordian <gordian.czerkawski@icloud.com>
2021-06-08 11:50:44 +0200

    added dll

---
+++ L3/zad1_z_dll/zad1/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace zad1l3
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next;
+
+        public Node()
+        {
+            next = null;
+            value = default(T);
+        }
+    }
+
+
+    public class Lista<T>
+    {
+        public Node<T> first;
+
+        public Lista()
+        {
+            first = null;
+        }
+
+        public bool IsEmpty()
+        {
+            if (first == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void push_front(T elem)
+        {
+            if (first == null) //gdy lista jest pusta
+            {
+                first = new Node<T>();
+                first.value = elem;
+            }
+            else
+            {
+                Node<T> n = new Node<T>();
+                n.value = elem;
+                n.next = first;
+                first = n;
+            }
+        }
+
+        public void push_end(T elem)
+        {
+            if (first == null) //gdy lista jest pusta
+            {
+                first = new Node<T>();
+                first.value = elem;
+            }
+            else
+            {
+                Node<T> n = new Node<T>();
+                n.value = elem;
+                if (first.next == null)
+                {
+                    first.next = n;
+                }
+                else
+                {
+                    Node<T> temp = first;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = n;
+                }
+            }
+        }
+        public T pop_front()
+        {
+            if (first == null) //gdy lista jest pusta
+            {
+                Console.WriteLine("Lista pusta!");
+                return default(T);
+            }
+            else
+            {
+                T temp = first.value;
+                first = first.next;
+                return temp;
+            }
+        }
+
+        public T pop_back()
+        {
+            T tempval;
+            Node<T> temp = first;
+
+            if (first == null) //gdy lista jest pusta
+            {
+                Console.WriteLine("Lista pusta");
+                return default(T);
+            }
+            else
+            {
+                if (temp.next == null)
+                {
+                    tempval = temp.value;
+                    temp = null;
+                    return tempval;
+                }
+                while (temp.next.next != null)
+                {
+                    temp = temp.next;
+                }
+                tempval = temp.next.value;
+                temp.next = null;
+                return tempval;
+            }
+        }
+    }
+
+
+
+    class Testy
+    {
+        static void Main(string[] args)
+        {
+            Lista<int> obiekt = new Lista<int>();
+            Console.WriteLine("TEST");
+            Console.WriteLine("Wynik wywolania IsEmpty dla pustej:");
+            Console.WriteLine(obiekt.IsEmpty());
+            Console.WriteLine("Dodajemy 7 na koniec");
+            obiekt.push_front(7);
+            Console.WriteLine("Dodajemy 3 na koniec");
+            obiekt.push_end(3);
+            Console.WriteLine("Dodajemy 1 na poczatek");
+            obiekt.push_front(1);
+            Console.WriteLine("Dodajemy 15 na poczatek");
+            obiekt.push_front(15);
+            Console.WriteLine("Dodajemy 4 na koniec");
+            obiekt.push_end(4);
+            Console.WriteLine("Wynik wywolania IsEmpty niepustej:");
+            Console.WriteLine(obiekt.IsEmpty());
+            Console.WriteLine("Usuwamy 1. element");
+            Console.WriteLine(obiekt.pop_front());
+            Console.WriteLine("Usuwamy ostatni element");
+            Console.WriteLine(obiekt.pop_back());
+        }
+    }
+}
---
+++ L3/zad2_z_dll/zad2/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace z2l3
+{
+    public class slownik<K, V> where K : IComparable<K>
+    {
+        slownik<K, V> next;
+        protected K key;
+        protected V val;
+
+        public slownik()
+        {
+            next = null;
+            key = default(K);
+            val = default(V);
+        }
+
+        public void Add(K key, V val)
+        {
+            if (this.next != null)
+            {
+                this.next.Add(key, val); //dochodzimy rekurencyjnie na koniec slownika
+            }
+            else
+            {
+                this.next = new slownik<K, V>();
+                this.next.key = key;
+                this.next.val = val;
+            }
+
+        }
+        public void Delete(K key)
+        {
+            if (key.CompareTo(this.next.key) == 0) //nastepny to klucz ktory chcemy usunac
+            {
+                this.next = this.next.next;
+            }
+            else
+            {
+                if (this.next != null)
+                    this.next.Delete(key); //przechodzimy dalej rekurencyjnie
+            }
+        }
+
+        public V Find(K key)
+        {
+            slownik<K, V> current = this;
+            while (key.CompareTo(current.key) != 0) //wychodzimy z petli gdy current to szukane haslo
+            {
+                current = current.next;
+                if (current == null)
+                    return default(V);
+            }
+            return current.val;
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Dictionary = new slownik<int, string>();
+            Dictionary.Add(53, "pies");
+            Dictionary.Add(65, "kot");
+            Dictionary.Add(79, "owca");
+            Dictionary.Delete(53);
+            System.Console.WriteLine(Dictionary.Find(53));
+            System.Console.WriteLine(Dictionary.Find(65));
+            System.Console.WriteLine(Dictionary.Find(79));
+        }
+    }
+}
